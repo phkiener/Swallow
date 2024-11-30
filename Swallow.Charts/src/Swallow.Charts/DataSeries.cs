fa56325de917c54a73611dad67483c9163e50d0f
Philipp Kiener <kiener.phil@gmail.com>
2024-11-30 12:07:45 +0100

    The dots work

---
+++ Swallow.Charts/src/Swallow.Charts/DataSeries.cs
@@ -0,0 +1,7 @@
+﻿using System.Numerics;
+
+namespace Swallow.Charts;
+
+public sealed record DataSeries<TKey, TValue>(string Name, IEnumerable<KeyValuePair<TKey, TValue>> Values)
+    where TKey : IEquatable<TKey>, IComparable<TKey>
+    where TValue : INumber<TValue>;
---
+++ Swallow.Charts/src/Swallow.Charts/LineChart.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using Microsoft.AspNetCore.Components;
+
+namespace Swallow.Charts;
+
+/// <summary>
+/// A chart representing one or more <see cref="DataSeries{TKey,TValue}"/> as lines.
+/// </summary>
+/// <typeparam name="TKey">Type of the key of each data point.</typeparam>
+/// <typeparam name="TValue">Type of the value.</typeparam>
+public sealed partial class LineChart<TKey, TValue> : ComponentBase
+    where TKey : IEquatable<TKey>, IComparable<TKey>
+    where TValue : INumber<TValue>
+{
+    [Parameter]
+    [EditorRequired]
+    public required IEnumerable<DataSeries<TKey, TValue>> Data { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?> AdditionalAttributes { get; set; } = new Dictionary<string, object?>();
+
+    private TValue maxValue = TValue.Zero;
+    private int valueCount = 0;
+
+    protected override void OnParametersSet()
+    {
+        maxValue = Data.Max(static series => series.Values.Max(static value => value.Value)) ?? TValue.Zero;
+        valueCount = Data.Max(static series => series.Values.Count());
+    }
+}
+
