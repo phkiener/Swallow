74f8d71da71268d73138befaebbf97fc4f7be146
Philipp Kiener <kiener.phil@gmail.com>
2025-01-18 22:35:02 +0100

    Simple binding

--- Swallow.Console/src/Swallow.Console.Arguments/ArgParse.cs
+++ Swallow.Console/src/Swallow.Console.Arguments/ArgParse.cs
@@ -4,6 +4,52 @@ public static class ArgParse
 {
     public static T Parse<T>(string[] args)
     {
-        throw new NotImplementedException();
+        var constructors = typeof(T).GetConstructors().OrderByDescending(static c => c.GetParameters().Length);
+
+        foreach (var constructor in constructors)
+        {
+            var parameters = constructor.GetParameters();
+            var arguments = new object?[parameters.Length];
+            var isMatch = true;
+
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                if (!TryConvert(args[i], parameters[i].ParameterType, out object? value))
+                {
+                    isMatch = false;
+                    break;
+                }
+
+                arguments[i] = value;
+            }
+
+            if (isMatch)
+            {
+                return (T)constructor.Invoke(arguments);
+            }
+        }
+
+        throw new InvalidOperationException("No suitable constructor found.");
+    }
+
+    private static bool TryConvert(string argument, Type targetType, out object? value)
+    {
+        if (targetType == typeof(int))
+        {
+            if (int.TryParse(argument, out var result))
+            {
+                value = result;
+                return true;
+            }
+        }
+
+        if (targetType == typeof(string))
+        {
+            value = argument;
+            return true;
+        }
+
+        value = null;
+        return false;
     }
 }
--- Swallow.Console/test/Swallow.Console.Test/Arguments/ArgumentParserTest.cs
+++ Swallow.Console/test/Swallow.Console.Test/Arguments/ArgumentParserTest.cs
@@ -3,9 +3,31 @@ namespace Swallow.Console.Arguments;
 [TestFixture]
 public sealed class ArgumentParserTest
 {
+    private sealed record EmptyOptions;
+
     [Test]
-    public void CanBeInvoked()
+    public void EmptyOptionsCanBeParsed()
     {
-        Assert.Throws<NotImplementedException>(() => ArgParse.Parse<int>([""]));
+        var options = ArgParse.Parse<EmptyOptions>([]);
+        Assert.That(options, Is.EqualTo(new EmptyOptions()));
     }
+
+    private sealed record SingleArgument(string Value);
+
+    [Test]
+    public void SingleArgumentCanBeParsed()
+    {
+        var options = ArgParse.Parse<SingleArgument>(["a"]);
+        Assert.That(options, Is.EqualTo(new SingleArgument("a")));
+    }
+
+    private sealed record SingleIntArgument(int Value);
+
+    [Test]
+    public void SingleArgumentWithTypeConversionCanBeParsed()
+    {
+        var options = ArgParse.Parse<SingleIntArgument>(["123"]);
+        Assert.That(options, Is.EqualTo(new SingleIntArgument(123)));
+    }
+
 }
